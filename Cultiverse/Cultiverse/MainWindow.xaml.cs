157b6a5fa08a6e6e458f893b77c7d4a9d603e396
Simon Nilsson <sim.nilsson@gmail.com>
2015-05-29 21:04:16 +0200

    Created new version of token sensor

--- Cultiverse/Cultiverse/MainWindow.xaml.cs
+++ Cultiverse/Cultiverse/MainWindow.xaml.cs
@@ -47,20 +47,6 @@ namespace Cultiverse
             createWorldView.saveCheck2Border.Visibility = Visibility.Hidden;
             createWorldView.saveCheck3Border.Visibility = Visibility.Hidden;
             createWorldView.saveCheck4Border.Visibility = Visibility.Hidden;
-
-            TouchExtensions.AddHoldGestureHandler(ellipse1, ellipse1_TouchDown);
-            TouchExtensions.AddHoldGestureHandler(label1, ellipse1_TouchDown);
-
-            TouchExtensions.AddHoldGestureHandler(ellipse2, ellipse2_TouchDown);
-            TouchExtensions.AddHoldGestureHandler(label2, ellipse2_TouchDown);
-
-            TouchExtensions.AddHoldGestureHandler(ellipse3, ellipse3_TouchDown);
-            TouchExtensions.AddHoldGestureHandler(label3, ellipse3_TouchDown);
-
-            TouchExtensions.AddHoldGestureHandler(ellipse4, ellipse4_TouchDown);
-            TouchExtensions.AddHoldGestureHandler(label4, ellipse4_TouchDown);
-
-            CompositionTarget.Rendering += update;
         }
         /// <summary>
         /// Occurs when the window is about to close. 
@@ -141,163 +127,8 @@ namespace Cultiverse
         }
 
 
-        float sizeMul, ticker;
-        public void update(object sender, EventArgs e)
-        {
-            sizeMul = (float)Math.Abs(Math.Sin(ticker++ / 100) * 0.2f) + 1.0f;
-            /*
-            ellipse1.Width = 150 * sizeMul;
-            ellipse1.Height = 150 * sizeMul;
-            ellipse2.Width = 150 * sizeMul;
-            ellipse2.Height = 150 * sizeMul;
-            ellipse3.Width = 150 * sizeMul;
-            ellipse3.Height = 150 * sizeMul;
-            ellipse4.Width = 150 * sizeMul;
-            ellipse4.Height = 150 * sizeMul;
-            */
-            Matrix sizingMatrix = Matrix.Identity;
-            sizingMatrix.ScaleAt(sizeMul, sizeMul, 150 / 2, 150 / 2);
-
-            ellipse1.RenderTransform = new MatrixTransform(sizingMatrix);
-            ellipse2.RenderTransform = new MatrixTransform(sizingMatrix);
-            ellipse3.RenderTransform = new MatrixTransform(sizingMatrix);
-            ellipse4.RenderTransform = new MatrixTransform(sizingMatrix);
-
-            //Canvas.SetLeft(ellipse1, 354 - ellipse1.Width/2);
-            //Canvas.SetTop(ellipse1, 400 - ellipse1.Height / 2);
-            RotateTransform rt1 = new RotateTransform();
-            RotateTransform rt2 = new RotateTransform();
-            RotateTransform rt3 = new RotateTransform();
-            RotateTransform rt4 = new RotateTransform();
-
-            rt1.Angle = 45;
-            rt2.Angle = 135;
-            rt3.Angle = 225;
-            rt4.Angle = 315;
-
-            ellipse1.LayoutTransform = rt1;
-            ellipse2.LayoutTransform = rt2;
-            ellipse3.LayoutTransform = rt3;
-            ellipse4.LayoutTransform = rt4;
-            label1.LayoutTransform = rt1;
-            label2.LayoutTransform = rt2;
-            label3.LayoutTransform = rt3;
-            label4.LayoutTransform = rt4;
-        }
 
         bool worldCreated = false;
-        int e1ActivateID = -9999;
-        private void ellipse1_TouchDown(object sender, TouchEventArgs e)
-        {
-            if (!worldCreated)
-            {
-                createWorldView.setWorld(worldDatabase.createNewWorld());
-                worldCreated = true;
-            }
-
-            if (createWorldView.drawingSpace2.Visibility == Visibility.Hidden)
-            {
-                createWorldView.Visibility = Visibility.Visible;
-                universeView.Visibility = Visibility.Hidden;
-
-                createWorldView.drawingSpace2.Visibility = Visibility.Visible;
-                createWorldView.saveCheck2Border.Visibility = Visibility.Visible;
-            }
-        }
-
-        private void ellipse1_TouchUp(object sender, TouchEventArgs e)
-        {
-            if (createWorldView.drawingSpace2.Visibility == Visibility.Visible)
-            {
-                createWorldView.drawingSpace2.Visibility = Visibility.Hidden;
-                createWorldView.saveCheck2Border.Visibility = Visibility.Hidden;
-                e1ActivateID = -9999;
-            }
-        }
-
-        private void ellipse2_TouchDown(object sender, TouchEventArgs e)
-        {
-            if (!worldCreated)
-            {
-                createWorldView.setWorld(worldDatabase.createNewWorld());
-                worldCreated = true;
-            }
-
-            if (createWorldView.drawingSpace1.Visibility == Visibility.Hidden)
-            {
-                createWorldView.Visibility = Visibility.Visible;
-                universeView.Visibility = Visibility.Hidden;
-
-                createWorldView.drawingSpace1.Visibility = Visibility.Visible;
-                createWorldView.saveCheck1Border.Visibility = Visibility.Visible;
-            }
-        }
-
-        private void ellipse2_TouchUp(object sender, TouchEventArgs e)
-        {
-            if (createWorldView.drawingSpace1.Visibility == Visibility.Visible)
-            {
-                createWorldView.drawingSpace1.Visibility = Visibility.Hidden;
-                createWorldView.saveCheck1Border.Visibility = Visibility.Hidden;
-                e1ActivateID = -9999;
-            }
-        }
-
-        private void ellipse3_TouchDown(object sender, TouchEventArgs e)
-        {
-            if (!worldCreated)
-            {
-                createWorldView.setWorld(worldDatabase.createNewWorld());
-                worldCreated = true;
-            }
-
-            if (createWorldView.drawingSpace4.Visibility == Visibility.Hidden)
-            {
-                createWorldView.Visibility = Visibility.Visible;
-                universeView.Visibility = Visibility.Hidden;
-
-                createWorldView.drawingSpace4.Visibility = Visibility.Visible;
-                createWorldView.saveCheck4Border.Visibility = Visibility.Visible;
-            }
-        }
-
-        private void ellipse3_TouchUp(object sender, TouchEventArgs e)
-        {
-            if (createWorldView.drawingSpace4.Visibility == Visibility.Visible)
-            {
-                createWorldView.drawingSpace4.Visibility = Visibility.Hidden;
-                createWorldView.saveCheck4Border.Visibility = Visibility.Hidden;
-                e1ActivateID = -9999;
-            }
-        }
-
-        private void ellipse4_TouchDown(object sender, TouchEventArgs e)
-        {
-            if (!worldCreated)
-            {
-                createWorldView.setWorld(worldDatabase.createNewWorld());
-                worldCreated = true;
-            }
-
-            if (createWorldView.drawingSpace3.Visibility == Visibility.Hidden)
-            {
-                createWorldView.Visibility = Visibility.Visible;
-                universeView.Visibility = Visibility.Hidden;
-
-                createWorldView.drawingSpace3.Visibility = Visibility.Visible;
-                createWorldView.saveCheck3Border.Visibility = Visibility.Visible;
-            }
-        }
-
-        private void ellipse4_TouchUp(object sender, TouchEventArgs e)
-        {
-            if (createWorldView.drawingSpace3.Visibility == Visibility.Visible)
-            {
-                createWorldView.drawingSpace3.Visibility = Visibility.Hidden;
-                createWorldView.saveCheck3Border.Visibility = Visibility.Hidden;
-                e1ActivateID = -9999;
-            }
-        }
 
         private void createWorldView_CreateWorldDone(object sender, RoutedEventArgs e)
         {
@@ -325,6 +156,117 @@ namespace Cultiverse
 
                 createWorldView.drawingSpace2.Visibility = Visibility.Visible;
                 createWorldView.saveCheck2Border.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void tokenSensor1_TokenDown(object sender, RoutedEventArgs e)
+        {
+            if (!worldCreated)
+            {
+                createWorldView.setWorld(worldDatabase.createNewWorld());
+                worldCreated = true;
+            }
+
+            if (createWorldView.drawingSpace1.Visibility == Visibility.Hidden)
+            {
+                createWorldView.Visibility = Visibility.Visible;
+                universeView.Visibility = Visibility.Hidden;
+
+                createWorldView.drawingSpace1.Visibility = Visibility.Visible;
+                createWorldView.saveCheck1Border.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void tokenSensor1_TokenUp(object sender, RoutedEventArgs e)
+        {
+            if (createWorldView.drawingSpace1.Visibility == Visibility.Visible)
+            {
+                createWorldView.drawingSpace1.Visibility = Visibility.Hidden;
+                createWorldView.saveCheck1Border.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void tokenSensor2_TokenDown(object sender, RoutedEventArgs e)
+        {
+            if (!worldCreated)
+            {
+                createWorldView.setWorld(worldDatabase.createNewWorld());
+                worldCreated = true;
+            }
+
+            if (createWorldView.drawingSpace2.Visibility == Visibility.Hidden)
+            {
+                createWorldView.Visibility = Visibility.Visible;
+                universeView.Visibility = Visibility.Hidden;
+
+                createWorldView.drawingSpace2.Visibility = Visibility.Visible;
+                createWorldView.saveCheck2Border.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void tokenSensor2_TokenUp(object sender, RoutedEventArgs e)
+        {
+
+            if (createWorldView.drawingSpace2.Visibility == Visibility.Visible)
+            {
+                createWorldView.drawingSpace2.Visibility = Visibility.Hidden;
+                createWorldView.saveCheck2Border.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void tokenSensor3_TokenDown(object sender, RoutedEventArgs e)
+        {
+            if (!worldCreated)
+            {
+                createWorldView.setWorld(worldDatabase.createNewWorld());
+                worldCreated = true;
+            }
+
+            if (createWorldView.drawingSpace3.Visibility == Visibility.Hidden)
+            {
+                createWorldView.Visibility = Visibility.Visible;
+                universeView.Visibility = Visibility.Hidden;
+
+                createWorldView.drawingSpace3.Visibility = Visibility.Visible;
+                createWorldView.saveCheck3Border.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void tokenSensor3_TokenUp(object sender, RoutedEventArgs e)
+        {
+
+            if (createWorldView.drawingSpace3.Visibility == Visibility.Visible)
+            {
+                createWorldView.drawingSpace3.Visibility = Visibility.Hidden;
+                createWorldView.saveCheck3Border.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void tokenSensor4_TokenDown(object sender, RoutedEventArgs e)
+        {
+            if (!worldCreated)
+            {
+                createWorldView.setWorld(worldDatabase.createNewWorld());
+                worldCreated = true;
+            }
+
+            if (createWorldView.drawingSpace4.Visibility == Visibility.Hidden)
+            {
+                createWorldView.Visibility = Visibility.Visible;
+                universeView.Visibility = Visibility.Hidden;
+
+                createWorldView.drawingSpace4.Visibility = Visibility.Visible;
+                createWorldView.saveCheck4Border.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void tokenSensor4_TokenUp(object sender, RoutedEventArgs e)
+        {
+
+            if (createWorldView.drawingSpace4.Visibility == Visibility.Visible)
+            {
+                createWorldView.drawingSpace4.Visibility = Visibility.Hidden;
+                createWorldView.saveCheck4Border.Visibility = Visibility.Hidden;
             }
         }
     }
---
+++ Cultiverse/Cultiverse/TokenSensor.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Surface.Presentation.Input;
+
+namespace Cultiverse
+{
+    /// <summary>
+    /// Interaction logic for TokenSensor.xaml
+    /// </summary>
+    public partial class TokenSensor : UserControl
+    {
+        public event RoutedEventHandler TokenDown;
+        public event RoutedEventHandler TokenUp;
+
+        public TokenSensor()
+        {
+            InitializeComponent();
+            CompositionTarget.Rendering += update;
+        }
+
+        private void ellipse_TouchDown(object sender, TouchEventArgs e)
+        {
+            // Capture the touch device and handle the event 
+            IInputElement element = sender as IInputElement;
+            if (element != null && e.Device.Capture(element))
+            {
+                TokenDown(sender, e);
+                e.Handled = true;
+            }
+        }
+
+        private void ellipse_LostTouchCapture(object sender, TouchEventArgs e)
+        {
+            TokenUp(sender, e);
+        }
+
+
+        float sizeMul, ticker;
+        public void update(object sender, EventArgs e)
+        {
+            sizeMul = (float)Math.Abs(Math.Sin(ticker++ / 100) * 0.2f) + 0.8f;
+            Matrix sizingMatrix = Matrix.Identity;
+            sizingMatrix.ScaleAt(sizeMul, sizeMul, ellipse.Width / 2, ellipse.Height / 2);
+
+            ellipse.RenderTransform = new MatrixTransform(sizingMatrix);
+        }
+    }
+}
