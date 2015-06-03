98ce3c47bb9043415460ce9858d67642cbda15f5
Digohd <Digohd1337@gmail.com>
2015-06-03 19:51:58 +0200

    Green Buttons Fix

--- Cultiverse/Cultiverse/MainWindow.xaml.cs
+++ Cultiverse/Cultiverse/MainWindow.xaml.cs
@@ -144,6 +144,8 @@ namespace Cultiverse
             Planet planet = universeView.addWorld(createWorldView.currentWorld);
             worldDatabase.saveWorld(createWorldView.currentWorld);
 
+            createWorldView.clearCreateWorldCanvas();
+
             createWorldView.Hide();
             //universeView.Visibility = Visibility.Visible;
 
